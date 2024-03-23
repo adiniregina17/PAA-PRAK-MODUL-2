c8d9ed188ce3255e0c3f73cd74377de53ab99705
adiniregina17 <117794795+adiniregina17@users.noreply.github.com>
2024-03-23 21:07:56 +0700

    Add files via upload

---
+++ AksesDb/Controllers/PersonController.cs
@@ -0,0 +1,26 @@
+﻿using AksesDb.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AksesDb.Controllers
+{
+    public class PersonController : Controller
+    {
+        private string __constr;
+        public PersonController(IConfiguration configuration)
+        {
+            __constr = configuration.GetConnectionString("WebApiDatabase");
+        }
+        public IActionResult index()
+        {
+            return View();
+        }
+        [HttpGet("api/person")]
+        public ActionResult<Person> ListPerson()
+        {
+            PersonContext context = new PersonContext(__constr);
+            List<Person> ListPerson = context.ListPerson();
+            return Ok(ListPerson);
+        }
+    }
+}
---
+++ AksesDb/Models/PersonContext.cs
@@ -0,0 +1,45 @@
+﻿using Npgsql;
+using AksesDb.Helpers;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AksesDb.Models
+{
+    public class PersonContext
+    {
+        private string __constr;
+        private string __ErrorMsg;
+        public PersonContext(string pConstr)
+        {
+            __constr = pConstr;
+        }
+        public List<Person> ListPerson()
+        {
+            List<Person> list1 = new List<Person>();
+            string query = string.Format(@"SELECT id_person, nama, alamat, email FROM users.person;");
+            SqlDBHelper db = new SqlDBHelper(this.__constr);
+            try
+            {
+                NpgsqlCommand cmd = db.GetNpgsqlCommand(query);
+                NpgsqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    list1.Add(new Person()
+                    {
+                        id_person = int.Parse(reader["id_person"].ToString()),
+                        nama = reader["nama"].ToString(),
+                        alamat = reader["alamat"].ToString(),
+                        email = reader["email"].ToString(),
+                    });
+                }
+                cmd.Dispose();
+                db.closeConnection();
+            }
+            catch (Exception ex)
+            {
+                __ErrorMsg = ex.Message;
+            }
+            return list1;
+        }
+    }
+}
