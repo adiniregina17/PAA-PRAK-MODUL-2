f19ca8c3528c768cca646cfc29690c41d35ab240
adiniregina17 <117794795+adiniregina17@users.noreply.github.com>
2024-03-23 21:09:45 +0700

    Add files via upload

---
+++ aksesdb_mandiri/aksesdb_mandiri/Models/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace aksesdb_mandiri.Models
+{
+    public class Murid
+    {
+        public int id_person { get; set; }
+        public string nama { get; set; }
+        public string alamat { get; set; }
+        public string email { get; set; }
+    }
+}
